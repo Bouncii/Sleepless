0fdb4a39f95cbab50a224f0150f0d1897c770f31
Bouncii <lulubouncer@gmail.com>
2025-09-23 10:41:37 +0200

    docstring des fonctions actuelles

--- prototype.py
+++ prototype.py
@@ -22,7 +22,7 @@ level_str = [["sol" for j in range (7)] for i in range (3)]
 
 
 class Player:
-  def __init__ (self,grid_x:int,grid_y:int):
+  def __init__ (self, grid_x:int, grid_y:int):
 
     self.grid_x = grid_x
     self.grid_y = grid_y
@@ -37,20 +37,34 @@ class Player:
     self.height = 70
     self.width = 50
  
-  
+
   def move(self):
+    '''
+    Fonction qui détecte une pression des touches et agit en conséquence
+    entrées: none
+    sorties: none
+    '''
     if self.pixel_x == self.target_x and self.pixel_y == self.target_y:
-      keys = pygame.key.get_pressed()
-      if keys[pygame.K_z]:
-          self.try_move(0,-1)
-      if keys[pygame.K_s]:
-          self.try_move(0,1)
-      if keys[pygame.K_q]:
-          self.try_move(-1,0)
-      if keys[pygame.K_d]:
-          self.try_move(1,0)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_z]:
+            self.try_move(0,-1)
+        if keys[pygame.K_s]:
+            self.try_move(0,1)
+        if keys[pygame.K_q]:
+            self.try_move(-1,0)
+        if keys[pygame.K_d]:
+            self.try_move(1,0)
   
   def try_move(self,dx:int,dy:int):
+      '''
+      Fonction qui vérifie si le déplacement est possible
+      si faisable : update target x et y pour déplacement et animation
+      si pas faisable : ne fait rien
+      entrées: 
+        dx : int  
+        dy : int  
+      sorties: none
+      '''
       new_x = self.grid_x + dx
       new_y = self.grid_y + dy
       if 0 <= new_x and new_x < GRID_WIDTH and 0 <= new_y and new_y < GRID_HEIGHT:
@@ -60,6 +74,13 @@ class Player:
           self.target_y = self.grid_y * TILE_SIZE
 
   def update(self, dt):
+    '''
+    Fonction qui actualise le déplacement/animation
+    vérifie si x et y target ne sont pas atteints, si pas atteints alors on incrémente la co avec speed
+    entrées: 
+    dt : float
+    sorties: none
+    '''
     if self.pixel_x < self.target_x:
         self.pixel_x += self.speed * dt
         if self.pixel_x > self.target_x:
@@ -78,16 +99,39 @@ class Player:
         if self.pixel_y < self.target_y:
             self.pixel_y = self.target_y
 
-  def show(self,tile_size):
+  def show(self,tile_size:int):
+      '''
+      Fonction qui dessine le joueur en fonction de ses attributs
+      entrées: 
+        tile_size :int
+      sorties: none
+      '''
       pygame.draw.rect(screen, "red", (self.pixel_x, self.pixel_y, self.width, self.height ))
 
 
 #################################### Map ####################################
 
 def build_sol(pos_tile_pixel:tuple, tile_dimension:tuple)->pygame.Rect:
+    '''
+    Fonction qui construit une structure de type sol
+    entrées: 
+        pos_tile_pixel : tuple des coordonnées de la tile en pixel
+        tile_dimension : tuple  des dimension de la tile
+    sorties: 
+        un dictionnaire contenant un rect pygame (la fome geometrique) et la couleur en rvb
+    '''
     return {"rect":pygame.Rect(pos_tile_pixel[0], pos_tile_pixel[1]+(0.8*tile_dimension[1]),tile_dimension[0],tile_dimension[1]*0.2),"color":(255,0,50)}
 
 def structures_builder(tile_type:str,pos_tile_pixel:tuple,tile_dimension:tuple) -> list:
+    '''
+    Fonction qui construit un tableau avec toute les structures de la tile en fonction de son type
+    entrées: 
+        tile_type : str  
+        pos_tile_pixel : tuple  
+        tile_dimension : tuple  
+    sorties: 
+        la liste contenant les structures
+    '''
     res= []
     if tile_type == "sol":
         res.append(build_sol(pos_tile_pixel,tile_dimension))
@@ -106,13 +150,20 @@ class Tile:
         self.width = width
         self.height = height
 
+        self.background = (100,100,150)
+
         self.tile_type = tile_type # type de la tile (exemple : "sol", "échelle", "bouton", "vide")
 
         self.structures = structures_builder(self.tile_type,(self.pixel_x,self.pixel_y),(self.width,self.height)) #Tab contenant l'ensemble des structures (sous forme d'objet rect pygames) présentent dans la tile
 
 
     def draw(self):
-        pygame.draw.rect(screen,(100,100,100),(self.x, self.y, self.width, self.height))
+        '''
+        Fonction qui dessine la tile puis les structures qu'elle conttient
+        entrées: none
+        sorties: none
+        '''
+        pygame.draw.rect(screen,self.background,(self.x, self.y, self.width, self.height))
         for structure in self.structures:
             pygame.draw.rect(screen,structure["color"],structure["rect"])
 
@@ -133,7 +184,17 @@ class Tile:
 
 
 
-def level_builder(grid_height:int,grid_width:int,tile_size:int,level_str:str):
+def level_builder(grid_height:int,grid_width:int,tile_size:int,level_str:str) -> list:
+    '''
+    Fonction qui construit le niveau sous forme de tableau 2 dimension d'objet Tile 
+    entrées: 
+        grid_height : int  
+        grid_width : int  
+        tile_size : int  
+        level_str : str  
+    sorties: 
+        res : list
+    '''
     res=[]
     for row in range(grid_height):
         tab_row = []
