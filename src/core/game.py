b9e00753542f8dfe674da9e528648f4dcb8e9fcf
bounci <lulubouncer@gmail.com>
2025-11-01 21:09:53 +0100

    reset quand player touche past self

--- src/core/game.py
+++ src/core/game.py
@@ -131,6 +131,9 @@ class Game:
             if are_all_entities_idle(self.player,self.past_self_tab):
                 self.update_buttons_state()
                 
+                #Verif meme case que past self
+                self.verif_same_tile_as_past_self()
+
                 # Vérification de la victoire
                 if self.player.on_finish():
                     self.state = GameState.WIN
@@ -170,6 +173,15 @@ class Game:
         for button_id in pressed_buttons:
             if button_id not in self.interactionManagerDoorButton.pressed_buttons:
                 self.interactionManagerDoorButton.button_pressed(button_id)
+
+    def verif_same_tile_as_past_self(self):
+        '''
+        Fonction qui vérifie si le joueur est sur la même case que past self et agit en conséquence
+        '''
+        for past_self in self.past_self_tab:
+            if past_self.grid_x == self.player.grid_x and past_self.grid_y == self.player.grid_y and past_self.timer_spawn == 0:
+                self.state = GameState.RESET_GAME
+
 
 
 
