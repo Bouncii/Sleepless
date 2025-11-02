b46aedc6a8ba226a2e7b70b96b10aa8905abea77
Nils <nilsbriffod@gmail.com>
2025-11-02 17:56:33 +0100

    Changement d'emplacement du fichier

---
+++ src/build/map_settings_functions.py
@@ -0,0 +1,14 @@
+# src\build\map_settings_functions.py
+import json
+PATH = "assets\\levels\\levels_settings.json"
+
+def get_settings_by_level_number(level_num:int):
+    """ La fonction renvoie un dictionnaire de tous les settings correspondant au numéro du niveau passé en paramètre :
+    Entrée : int
+    Sortie : dict
+    """
+    with open(PATH, "r") as file:
+        data = json.load(file)
+    return data["level" + str(level_num)]
+
+# Exemple : get_settings_by_level_number(1) -> {'past_self_timer_spawn': 2}S
