e64f3e9f9be9b8b3237200d7e32ba32de7a94f62
Nils <nilsbriffod@gmail.com>
2025-11-17 08:52:11 +0100

    Items JSON

--- src/build/map_settings_functions.py
+++ src/build/map_settings_functions.py
@@ -10,10 +10,12 @@ def get_settings_by_level_number(level_num:int):
     with open(Paths.SETTINGS, "r") as file:
         data = json.load(file)
     return data["level" + str(level_num)]
+    
 
 def get_number_of_level():
     with open(Paths.SETTINGS, "r") as file:
         data = json.load(file)
     return data["number_of_level"]
 
-# Exemple : get_settings_by_level_number(1) -> {'past_selfs': [{'past_self_timer_spawn': 8}, {'past_self_timer_spawn': 2}]}
+# Exemple : get_settings_by_level_number(1) -> {'past_selfs': [{'past_self_timer_spawn': 2}, {'past_self_timer_spawn': 7}], 'items': [{'eclair': [2, 2]}, {'teleporteur': [1, 3]}]}
+# Exemple : recuperer les items : get_settings_by_level_number(1)["items"] -> [{'eclair': [2, 2]}, {'teleporteur': [1, 3]}]
