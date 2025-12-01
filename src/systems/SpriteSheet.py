46711b3047e76ec02a0a26e3b46cae8079051484
Nils <nilsbriffod@gmail.com>
2025-12-01 12:47:26 +0100

    WIP Sprite Character V2

--- src/systems/SpriteSheet.py
+++ src/systems/SpriteSheet.py
@@ -10,12 +10,16 @@ class SpriteSheet():
     def __init__(self,image,number_of_animation):
         self.sheet = image
         self.nbr_animation = number_of_animation
-        self.dimension = (1,1)
-        self.frame_dimension = (1,1)
+        img = pygame.image.load(image)
+        self.dimension = img.get_size()
+        self.frame_dimension = (self.dimension[0]//number_of_animation, self.dimension[1])
     
-    def get_image(self,frame,width,height,scale):
-        image = pygame.Surface((width,height)).convert_alpha()
-        image.blit(self.sheet,(0,0),((frame*width),0,width,height))
-        image = pygame.transform.scale(image,(width*scale,height*scale))
+    def get_image(self,frame,scale):
+        image = pygame.Surface((self.dimension[0],self.dimension[1])).convert_alpha()
+        image.blit(self.sheet,(0,0),((frame*self.dimension[0]),0,self.dimension[0],self.dimension[1]))
+        image = pygame.transform.scale(image,(self.dimension[0]*scale,self.dimension[1]*scale))
         return image
-    
+
+    def draw(self, screen, player, frame_nbr):
+            image = self.get_image(frame_nbr,1)
+            screen.blit(image, player.rect.topleft)
